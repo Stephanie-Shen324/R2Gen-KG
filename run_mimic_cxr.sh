435e3c742d5671827fe8a0e9b8b942b100e79ee8
Stephanie-Shen324 <72006612+Stephanie-Shen324@users.noreply.github.com>
2021-05-24 16:28:28 +1000

    Update run_mimic_cxr.sh

--- run_mimic_cxr.sh
+++ run_mimic_cxr.sh
@@ -4,12 +4,12 @@ python main.py \
 --max_seq_length 100 \
 --threshold 10 \
 --batch_size 16 \
---epochs 30 \
---save_dir results/mimic_cxr \
 --step_size 1 \
 --gamma 0.8 \
 --pretrained models/mimic_gcnclassifier_v1_ones3_t0v1t2_lr1e-6_e10.pth \
---d_vf 1024 \
 --num_classes 36 \
+--epochs 30 \
+--d_vf 1024 \
+--save_dir results/mimic_cxr \
 --dataset_name mimic_cxr \
 --seed 456789
